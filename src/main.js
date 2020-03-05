61c9c06b5c44fa453ce3f35f0f4e2ba6b68731f7
lxy <lxy@cfanle.com>
2020-03-05 20:20:06 +0800

    使用element-ui&router组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
 
+// 日志的详细与否，生产环境、开发环境
 Vue.config.productionTip = false
 
+Vue.use(ElementUi)
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
+
+// 入口文件
+// 职责：做全局范围内的依赖和配置
+// 职责：创建一个VUE实例
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+// 配置路由
+
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: []
+})
+
+export default router
